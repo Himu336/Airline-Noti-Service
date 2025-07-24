d92ccf8d4550e57475ad2d3e3721af1c514433e1
Himansh <himanshbansal200521@gmail.com>
2025-07-24 16:38:10 +0530

    created nodemailer transport object

--- src/index.js
+++ src/index.js
@@ -3,7 +3,12 @@ const express = require('express');
 const { ServerConfig } = require('./config');
 const apiRoutes = require('./routes');
 
+const mailsender = require('./config/email-config');
+
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use('/api', apiRoutes);
 
